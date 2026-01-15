0c5db184a8871dcd5db98fd02f27f552342492d4
HamedAlemo <h.alemohammad@gmail.com>
2026-01-14 23:02:41 -0500

    updated data layers

--- gee_grid_analysis/datasets.py
+++ gee_grid_analysis/datasets.py
@@ -18,11 +18,20 @@ def load_datasets(grid):
         'flii': ee.Image("projects/flii-pipeline/assets/final_metric/FLII_final_metric_300m_2022").clip(grid),
         'gcp_jung': ee.Image("projects/ee-madibipacifique/assets/jung").clip(grid),
         'gcp_dinerstein': ee.Image("projects/ee-madibipacifique/assets/Dinesrtein").clip(grid),
-        'esp': ee.Image("projects/ee-madibipacifique/assets/ncp").clip(grid),
+        'esp': ee.Image("projects/ee-madibipacifique/assets/ncp").unmask().clip(grid),
         'climate': ee.Image("projects/ee-madibipacifique/assets/ClimateVulnerability_VF").clip(grid).select('b4'),
         'pa': ee.FeatureCollection("projects/earth-engine-071095/assets/Clark_Labs/WCS_Congo/DRC_Protected_Areas"),
         'kba': ee.FeatureCollection("projects/earth-engine-071095/assets/Clark_Labs/WCS_Congo/DRC_Key_Biodiversity_Areas"),
         'cf': ee.FeatureCollection("projects/earth-engine-071095/assets/Clark_Labs/WCS_Congo/DRC_Community_Forests"),
         'gc': ee.FeatureCollection("projects/ee-madibipacifique/assets/Green_Corridor_DRC"),
-        'oil': ee.FeatureCollection("projects/ee-madibipacifique/assets/Bloc_petrolier")
+        'oil': ee.FeatureCollection("projects/ee-madibipacifique/assets/Bloc_petrolier"),
+        'peat_thickness': ee.Image("projects/ee-pelsen-wcs/assets/ROC/Forest_properties/Crezee_2022_Median_Peat_thickness_RF_100runs").clip(grid),
+        'human_impact_index': ee.Image("projects/ee-pelsen-wcs/assets/ROC/Forest_properties/hii_roc_clip_2020").clip(grid),
+        'global_human_modification': ee.Image(ee.ImageCollection("CSP/HM/GlobalHumanModification").first()).clip(grid),
+        'anthropogenic_pressure': ee.Image("projects/ee-madibipacifique/assets/Anthropogenicpressure").clip(grid),
+        'nat_semi_grassland': ee.Image("projects/global-pasture-watch/assets/ggc-30m/v1/nat-semi-grassland_p/2022").clip(grid),
+        'peat_carbon': ee.Image("projects/ee-pelsen-wcs/assets/ROC/Forest_properties/Crezee_2022_Median_Carbon_density_2000runs").clip(grid),
+        'forest_ecosystems': ee.Image("projects/ee-pelsen-wcs/assets/ROC/Ecosystems_ecoregions/Shapiro_ecosystems").clip(grid),
+        'marine_pressure': ee.Image("users/pelsen_WCS/MCI/global_cumul_impact_2013_all_layers").clip(grid),
+        'biomass_carbon': ee.ImageCollection("NASA/ORNL/biomass_carbon_density/v1").mean().clip(grid)
     }
