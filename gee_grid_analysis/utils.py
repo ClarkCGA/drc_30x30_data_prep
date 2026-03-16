7d315ace4d3e4e370f91e5bc1fd5e60ab0166798
HamedAlemo <h.alemohammad@gmail.com>
2026-03-16 02:34:49 -0400

    added new hydrologic, biodiversity datasets

--- gee_grid_analysis/utils.py
+++ gee_grid_analysis/utils.py
@@ -10,7 +10,16 @@ def get_image_stats(image, grid, projection, property_name, scale):
     )
     return fractions.map(lambda f: f.set(property_name, f.get('mean')))
 
-def export_table(feature_collection, description, folder, selectors):
+def get_image_max(image, grid, projection, property_name, scale):
+    result = image.reduceRegions(
+        collection=grid,
+        reducer=ee.Reducer.max(),
+        scale=scale,
+        crs=projection
+    )
+    return result.map(lambda f: f.set(property_name, f.get('max')))
+
+def export_table(feature_collection, description, selectors, folder="DRC_30x30"):
     task = ee.batch.Export.table.toDrive(
         collection=feature_collection,
         description=description,
